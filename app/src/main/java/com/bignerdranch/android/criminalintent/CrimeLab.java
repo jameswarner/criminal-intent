20bce72f89afc74c033133d5917cbd8e27545b13
James Warner <frodo@frodos-lothlorien.net>
2015-12-15 23:13:08 -0500

    Add toolbar

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -25,6 +25,10 @@ public class CrimeLab {
         return mCrimes;
     }
 
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
+
     public Crime getCrime(UUID id) {
         for (Crime crime : mCrimes) {
             if (crime.getId().equals(id)) {
@@ -36,12 +40,6 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }
     }
 
 }
